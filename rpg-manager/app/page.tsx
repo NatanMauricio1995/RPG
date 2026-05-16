d347c804e91a99482cc10838f08255a3bff64bf6
Natan Mauricio Santos <natanmauriciosantos@exemplo.com>
2026-05-16 14:22:15 -0300

    PÁGINA PRINCIPAL - ATUALIZADA

--- rpg-manager/app/page.tsx
+++ rpg-manager/app/page.tsx
@@ -1,29 +1,14 @@
-import Topo from "../components/Topo/Topo";
-import Sidebar from "../components/Sidebar/Sidebar";
-
 export default function Home(){
 
 return(
 
-<div className="container">
-
-<Topo/>
-
-<div className="conteudo">
-
-<Sidebar/>
-
-<main className="principal">
+<div>
 
 <h1>🎲 RPG Manager</h1>
 
 <p>
 Bem-vindo ao gerenciador de mesa RPG
 </p>
-
-</main>
-
-</div>
 
 </div>
 
